ecdc9c61229e33defaa1e0ae8f7173ad54b8e00c
Guide Fari <guideg6@gmail.com>
2022-03-16 14:40:23 +0200

    feat: add configure amplify file

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "paulodichonecourse": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
