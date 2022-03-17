6218199b6408b4b431be098cefc6ff4502e5dd67
Guide <guideg6@gmail.com>
2022-03-17 13:13:17 +0200

    feat: some basic data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
+import { useState, useEffect } from "react"
+import { API } from "aws-amplify"
+import { listPosts } from "../src/graphql/queries.ts"
+
 export default function Home() {
-  return <h1 className="text-4xl font-bold  underline">Hello world!</h1>
+  const [posts, setPosts] = useState([])
+
+  async function fetchPosts() {
+    const postData = await API.graphql({
+      query: listPosts,
+    })
+
+    setPosts(postData.data.listPosts.items)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  return (
+    <div>
+      <h1 className="mb-20 text-4xl font-bold underline">My Posts</h1>
+      <div>
+        {posts.length > 0 &&
+          posts.map(post => <h4 key={post.id}>{post.title}</h4>)}
+      </div>
+    </div>
+  )
 }
